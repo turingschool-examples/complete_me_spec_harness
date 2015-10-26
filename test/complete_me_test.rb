209873301119caa7a605bd1c4134474eacfd63d1
Horace Williams <horace.d.williams@gmail.com>
2015-10-26 08:24:29 -0600

    sketching tests for spec harness

---
+++ test/complete_me_test.rb
@@ -0,0 +1,35 @@
+require "minitest"
+require "minitest/emoji"
+require "minitest/autorun"
+
+class CompleteMeTest
+  def test_inserts_single_word
+  end
+
+  def test_counts_inserted_words
+  end
+
+  def test_suggests_off_of_small_dataset
+  end
+
+  def test_inserts_medium_dataset
+  end
+
+  def test_counts_medium_dataset
+  end
+
+  def test_suggests_off_of_medium_dataset
+  end
+
+  def test_selects_off_of_medium_dataset
+  end
+
+  def test_subsequent_suggestions_account_for_selections
+  end
+
+  def test_inserts_large_dataset
+  end
+
+  def test_integration_test_over_large_dataset
+  end
+end
